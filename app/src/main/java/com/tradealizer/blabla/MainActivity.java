4e3da1afabb7b52457a0a79a70eb2bf9016f5ce7
AS <a.singh98@htl-ottakring.ac.at>
2017-04-10 15:32:18 +0200

    Navibar funktioniert

--- app/src/main/java/com/tradealizer/blabla/MainActivity.java
+++ app/src/main/java/com/tradealizer/blabla/MainActivity.java
@@ -5,6 +5,7 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AlertDialog;
@@ -66,6 +67,8 @@ public class MainActivity extends AppCompatActivity {
         mDrawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
 
+
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
@@ -79,6 +82,7 @@ public class MainActivity extends AppCompatActivity {
         // See https://g.co/AppIndexing/AndroidStudio for more information.
         client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
